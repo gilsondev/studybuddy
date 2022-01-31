adeb87cb49ac599eb7354963ab5d4e170b629fb6
Gilson Filho <me@gilsondev.in>
2022-01-31 20:33:29 -0300

    Creating basic templates

--- base/views.py
+++ base/views.py
@@ -2,9 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+rooms = [
+    {"id": 1, "name": "Lets learn python"},
+    {"id": 2, "name": "Design with me"},
+    {"id": 3, "name": "Frontend developer"},
+]
+
+
 def home(request):
-    return render(request, "home.html")
+    return render(request, "base/home.html", {"rooms": rooms})
 
 
-def room(request):
-    return render(request, "room.html")
+def room(request, pk):
+    room = next(filter(lambda item: item['id'] == pk, rooms), None)
+    return render(request, "base/room.html", {"room": room})
