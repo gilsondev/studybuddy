320223310296443f161dab75558cb9e4686f962b
Gilson Filho <me@gilsondev.in>
2022-02-02 17:01:50 -0300

    Room CRUD

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import get_object_or_404, redirect, render
+
+from base.forms import RoomForm
+from base.models import Room
+
 
 def home(request):
     rooms = Room.objects.all()
@@ -10,3 +12,36 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(pk=pk)
     return render(request, "base/room.html", {"room": room})
+
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("home")
+
+    return render(request, "base/room_form.html", {"form": form})
+
+
+def update_room(request, pk):
+    room = get_object_or_404(Room, id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid:
+            form.save()
+            return redirect("home")
+
+    return render(request, "base/room_form.html", {"form": form})
+
+
+def delete_room(request, pk):
+    room = get_object_or_404(Room, id=pk)
+
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": room})
