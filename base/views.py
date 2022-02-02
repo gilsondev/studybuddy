ed5da9de717e0a0ee40cf8f22e2cd0d7ae68e32e
Gilson Filho <me@gilsondev.in>
2022-02-02 15:45:16 -0300

    Models and Admin Panel

--- base/views.py
+++ base/views.py
@@ -1,18 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
-rooms = [
-    {"id": 1, "name": "Lets learn python"},
-    {"id": 2, "name": "Design with me"},
-    {"id": 3, "name": "Frontend developer"},
-]
-
+from .models import Room
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, "base/home.html", {"rooms": rooms})
 
 
 def room(request, pk):
-    room = next(filter(lambda item: item['id'] == pk, rooms), None)
+    room = Room.objects.get(pk=pk)
     return render(request, "base/room.html", {"room": room})
